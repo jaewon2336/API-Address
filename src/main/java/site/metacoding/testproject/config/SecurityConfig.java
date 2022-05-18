4c7a6af240490d3096ce93a20dc8cacaeebced97
jaewon2336 <xldzjqpf1588@naver.com>
2022-05-18 14:41:23 +0900

    로그인, 회원가입 페이지 완료

---
+++ src/main/java/site/metacoding/testproject/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package site.metacoding.testproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // super.configure(http); 부모 메서드 안쓸거야!!
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/s/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login-form")
+                .defaultSuccessUrl("/");
+    }
+
+}
--- src/main/java/site/metacoding/testproject/web/UserController.java
+++ src/main/java/site/metacoding/testproject/web/UserController.java
@@ -1,6 +1,7 @@
 package site.metacoding.testproject.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.testproject.service.UserService;
@@ -10,5 +11,20 @@ import site.metacoding.testproject.service.UserService;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/")
+    public String main() {
+        return "/user/main";
+    }
+
+    @GetMapping("join-form")
+    public String joinForm() {
+        return "/user/joinForm";
+    }
+
+    @GetMapping("login-form")
+    public String loginForm() {
+        return "/user/loginForm";
+    }
 
 }
